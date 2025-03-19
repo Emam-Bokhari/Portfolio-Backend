92628b874038dfdb1a7a714a55ec71964ede2d84
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-19 12:48:42 +0600

    feat(skill): add Mongoose schema for Skill model

---
+++ src/app/modules/Skill/skill.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import { TSkill } from "./skill.interface";
+
+const skillSchema = new Schema<TSkill>({
+    icon: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+)
+
+export const Skill = model<TSkill>("Skill", skillSchema)
